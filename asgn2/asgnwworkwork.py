bd7f8f6110e3f5d33ea484504af7cb0cdb1322d9
maxdickinson <max.dickinson58@gmail.com>
2016-04-28 14:40:39 -0700

    work
    work

--- asgn2/asgnwworkwork.py
+++ asgn2/asgnwworkwork.py
@@ -1,6 +1,7 @@
 import os
 from collections import defaultdict
 from collections import deque
+import sys
 #os.chdir("C:\\Users\\maxwell\\Documents\\cis315\\asgn2")
 ##
 def toposort(graph):
@@ -53,79 +54,80 @@ def longestpathDAG(graph, startnode, endnode):
     return dist[endnode], maxpath
 
 if __name__ == '__main__':
-with sys.stdin as fil:
-#file= open('samplein.py', 'r')
-    #a=file.readlines()
-    a=fil.readlines()
-    nodes=[]
-    all=[]
-    agg=[]
-    first=0
-    count=1
-    for line in a:
-        stop=len(a)
-        line2=line.rstrip("\n")
-        nums= line2.rsplit(' ')
-        n=nums[0:3]
-        if len(n)==1:
-            graphs=n
-            count+=1
-        elif len(n) == 2 and first ==1:
-            print('run1')
-            nodes.append(n)
-            all.append(agg)
-            agg=[]
-            count+=1
-        elif len(n) == 2:
-            nodes.append(n)
-            first=1
-            count+=1
-        elif len(n) == 3:#notworking add if  statement to look for same vertex here. 
-            agg.append(n)
-            if count == stop:
-                print('run2')
-                all.append(agg)
-            count+=1
-            
-    ##all has my two graphs
-    gr=[]
-    c=1
-    e=0
-    for g in all:
-        keys=[]
-        values=[]
-        weights=[]
-        for line in g:
-            vee=line
-            q=deque(vee)
-            key=int(q.popleft())
-            keys.append(key)
-            values.append(int(q.popleft()))
-            weights.append(int(q.popleft()))
-        d=dict.fromkeys(keys)
-        for i in range(1,len(d)+1):
-            #print(i)
-            lis=[]
-            for j in range(len(keys)):
-                #print(j)
-                if i==keys[j]:
-                    lis.append((values[j],weights[j]))
-                    #if 1=1zip
-                    #add valuesto lis 
-            #print(lis)
-            d[i] = lis 
-            
-        gr.append(d)
     
-        dist,path=longestpathDAG(gr[e],1,len(d)+1)
-        e+=1
-        print("Graph number is")
-        print(c)
-        print("longest path distance")
-        print(dist)
-        print("number os longest paths are")
-        print(path)
-        c+=1
+    fil=sys.argv[-1]
+    with open(fil,'r') as fil:
+        # #a=file.readlines()
+        a=fil.readlines()
+        nodes=[]
+        all=[]
+        agg=[]
+        first=0
+        count=1
+        for line in a:
+            stop=len(a)
+            line2=line.rstrip("\n")
+            nums= line2.rsplit(' ')
+            n=nums[0:3]
+            if len(n)==1:
+                graphs=n
+                count+=1
+            elif len(n) == 2 and first ==1:
+                print('run1')
+                nodes.append(n)
+                all.append(agg)
+                agg=[]
+                count+=1
+            elif len(n) == 2:
+                nodes.append(n)
+                first=1
+                count+=1
+            elif len(n) == 3:#notworking add if  statement to look for same vertex here. 
+                agg.append(n)
+                if count == stop:
+                    print('run2')
+                    all.append(agg)
+                count+=1
+                
+        ##all has my two graphs
+        gr=[]
+        c=1
+        e=0
+        for g in all:
+            keys=[]
+            values=[]
+            weights=[]
+            for line in g:
+                vee=line
+                q=deque(vee)
+                key=int(q.popleft())
+                keys.append(key)
+                values.append(int(q.popleft()))
+                weights.append(int(q.popleft()))
+            d=dict.fromkeys(keys)
+            for i in range(1,len(d)+1):
+                #print(i)
+                lis=[]
+                for j in range(len(keys)):
+                    #print(j)
+                    if i==keys[j]:
+                        lis.append((values[j],weights[j]))
+                        #if 1=1zip
+                        #add valuesto lis 
+                #print(lis)
+                d[i] = lis 
+                
+            gr.append(d)
+        
+            dist,path=longestpathDAG(gr[e],1,len(d)+1)
+            e+=1
+            print("Graph number is")
+            print(c)
+            print("longest path distance")
+            print(dist)
+            print("number os longest paths are")
+            print(path)
+            c+=1
 
     
     
